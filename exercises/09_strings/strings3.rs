7e5ea6492da1104cd61d890ec9c2c08c4c3dc080
jatin711-debug <jatinmahajan711@gmail.com>
2025-07-09 12:35:09 -0400

    Update exercises and solutions for structs, enums, and strings; enhance functionality and fix errors

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,21 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    // Here is my first approach
+    // let mut modified_string: String = String::from(input);
+    // modified_string.push_str(" world!");
+    // modified_string
+    let s = format!("{}{}", input, " world!");
+    s
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
