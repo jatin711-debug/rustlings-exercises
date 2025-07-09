d399d983c9f4c9f2f5ba4ae6eb57e4eeb3d56c4d
jatin711-debug <jatinmahajan711@gmail.com>
2025-07-09 03:22:53 -0400

    init russtling exercise

---
+++ exercises/02_functions/functions1.rs
@@ -0,0 +1,10 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+
+fn call_me() {
+    // This function should do nothing, but it must exist.
+    // You can leave it empty or add a comment.
+}
+
+fn main() {
+    call_me(); // Don't change this line
+}
